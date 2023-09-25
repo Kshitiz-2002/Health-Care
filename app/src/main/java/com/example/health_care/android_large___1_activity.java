10185786488bce57074048b2e13f34a2823201f5
Kshitiz-2002 <kshitizpandey2002@gmail.com>
2023-09-25 15:56:44 +0530

    Modified UI with Figma Design

---
+++ app/src/main/java/com/example/health_care/android_large___1_activity.java
@@ -0,0 +1,66 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		android_large___1
+	 *	@date 		Monday 25th of September 2023 06:48:02 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.health_care;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class android_large___1_activity extends Activity {
+
+	
+	private View _bg__android_large___1_ek2;
+	private View ellipse_1;
+	private View rectangle_2;
+	private View rectangle_4;
+	private View rectangle_3;
+	private View rectangle_1;
+	private TextView take_care_your_health;
+	private TextView get_started;
+	private TextView and_here_we_are_to_help_you_by_providing_best_services;
+	private ImageView jpeg_image_60dd96ac1ff2_1_1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.android_large___1);
+
+		
+		_bg__android_large___1_ek2 = (View) findViewById(R.id._bg__android_large___1_ek2);
+		ellipse_1 = (View) findViewById(R.id.ellipse_1);
+		rectangle_2 = (View) findViewById(R.id.rectangle_2);
+		rectangle_4 = (View) findViewById(R.id.rectangle_4);
+		rectangle_3 = (View) findViewById(R.id.rectangle_3);
+		rectangle_1 = (View) findViewById(R.id.rectangle_1);
+		take_care_your_health = (TextView) findViewById(R.id.take_care_your_health);
+		get_started = (TextView) findViewById(R.id.get_started);
+		and_here_we_are_to_help_you_by_providing_best_services = (TextView) findViewById(R.id.and_here_we_are_to_help_you_by_providing_best_services);
+		jpeg_image_60dd96ac1ff2_1_1 = (ImageView) findViewById(R.id.jpeg_image_60dd96ac1ff2_1_1);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
